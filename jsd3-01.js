f97e0f0c563b7d0178b12b512d1241ea7e91db07
JR Schmidt <jrschmidt56@gmail.com>
2016-02-06 13:06:22 -0800

    add alternating colors for bars

--- jsd3-01.js
+++ jsd3-01.js
@@ -1,35 +1,50 @@
 var data = [10, 15, 30, 50, 80, 65, 55, 30, 20, 10, 8];
+var oddeven = 0;
+var barColor = '#aacccc';
+
+render(data);
+
 
 function render(data) {
 
   // Enter
-  d3.select("body").selectAll("div.h-bar")
+  d3.select('body').selectAll('div.h-bar')
   .data(data)
   .enter()
-  .append("div")
-  .attr("class", "h-bar")
-  .append("span");
+  .append('div')
+  .attr('class', 'h-bar')
+  .append('span');
 
   // Update
-  d3.select("body").selectAll("div.h-bar")
+  d3.select('body').selectAll('div.h-bar')
   .data(data)
-  .style("width", function (d) {
-    console.log('d = ' + d);
-    return (d * 3) + "px";
+  .style('background-color', function() {
+    if (barColor === '#aaccaa')
+      {barColor = '#aacccc';}
+    else
+      {barColor = '#aaccaa';}
+    return barColor;
   })
-  .style("background-color", "#aaccaa")
-  .select("span")
+  .style('width', function (d) {
+    console.log('d = ' + d);
+    return (d * 3) + 'px';
+  })
+  .style('min-height', '12px')
+  .style('font-family', 'sans')
+  .style('font-size', '0.8em')
+  .select('span')
     .text(function (d) {
       console.log('d = ' + d);
       return d;
     });
 
   // Exit
-  d3.select("body").selectAll("div.h-bar")
+  d3.select('body').selectAll('div.h-bar')
   .data(data)
   .exit()
   .remove();
   }
+
 
 setInterval(
   function () {
@@ -39,5 +54,3 @@ setInterval(
   },
   1500
   );
-
-render(data);
