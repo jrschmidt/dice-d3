a3ee1732c0036e70e5944a4d9994f94c987619f8
JR Schmidt <jrschmidt56@gmail.com>
2016-02-24 18:27:44 -0800

    refactor RiskDiceProbabilities module, only include end results

--- modules/RiskDiceProbabilities.js
+++ modules/RiskDiceProbabilities.js
@@ -6,161 +6,57 @@ var RiskDiceProbabilities = (function () {
         var pgd3 = [0, 216, 215, 208, 189, 152, 91, 0];
         var pgdd2 = [0, 216, 150, 96, 54, 24, 6, 0];
         var pgdd3 = [0, 216, 200, 160, 108, 56, 16, 0];
-        var probs1v1 = [
-            [],
-            [180, 36],
-            [144, 72],
-            [108, 108],
-            [72, 144],
-            [36, 180],
-            [0, 216]
-        ];
-        var probs1v2 = probs1v1;
-        var probs2v1 = [
-            [],
-            [210, 6],
-            [192, 24],
-            [162, 54],
-            [120, 96],
-            [66, 150],
-            [0, 216]
-        ];
-        var probs3v1 = [
-            [],
-            [215, 1],
-            [208, 8],
-            [189, 27],
-            [152, 64],
-            [91, 125],
-            [0, 216]
-        ];
-        var probs2v2 = [
-            [],
-            [
-                [],
-                [150, 60, 6],
-            ],
-            [
-                [],
-                [144, 54, 18],
-                [96, 96, 24],
-            ],
-            [
-                [],
-                [126, 60, 30],
-                [90, 78, 48],
-                [54, 108, 54],
-            ],
-            [
-                [],
-                [96, 78, 42],
-                [72, 72, 72],
-                [48, 78, 90],
-                [24, 96, 96],
-            ],
-            [
-                [],
-                [54, 108, 54],
-                [42, 78, 96],
-                [30, 60, 126],
-                [18, 54, 144],
-                [6, 60, 150],
-            ],
-            [
-                [],
-                [0, 150, 66],
-                [0, 96, 120],
-                [0, 54, 162],
-                [0, 24, 192],
-                [0, 6, 210],
-                [0, 0, 216],
-            ]
-        ];
-        var probs3v2 = [
-            [],
-            [
-                [],
-                [200, 15, 1]
-            ],
-            [
-                [],
-                [196, 16, 4],
-                [160, 48, 8]
-            ],
-            [
-                [],
-                [180, 29, 7],
-                [153, 43, 20],
-                [108, 81, 27]
-            ],
-            [
-                [],
-                [146, 60, 10],
-                [128, 56, 32],
-                [98, 64, 54],
-                [56, 96, 64]
-            ],
-            [
-                [],
-                [88, 115, 13],
-                [79, 93, 44],
-                [64, 71, 81],
-                [43, 61, 112],
-                [16, 75, 125]
-            ],
-            [
-                [],
-                [0, 200, 16],
-                [0, 160, 56],
-                [0, 108, 108],
-                [0, 56, 160],
-                [0, 16, 200],
-                [0, 0, 216]
-            ]
-        ];
     }
+    RiskDiceProbabilities.prototype.getProbs1v1 = function () {
+        var pw = 15 / 36;
+        var pl = 21 / 36;
+        return [pw, pl];
+    };
+    RiskDiceProbabilities.prototype.getProbs1v2 = function () {
+        var pw = 55 / 216;
+        var pl = 161 / 216;
+        return [pw, pl];
+    };
+    RiskDiceProbabilities.prototype.getProbs2v1 = function () {
+        var pw = 125 / 216;
+        var pl = 91 / 216;
+        return [pw, pl];
+    };
+    RiskDiceProbabilities.prototype.getProbs2v2 = function () {
+        var pww = 295 / 1296;
+        var pwl = 420 / 1296;
+        var pll = 581 / 1296;
+        return [pww, pwl, pll];
+    };
+    RiskDiceProbabilities.prototype.getProbs3v1 = function () {
+        var pw = 855 / 1296;
+        var pl = 441 / 1296;
+        return [pw, pl];
+    };
+    RiskDiceProbabilities.prototype.getProbs3v2 = function () {
+        var pww = 2890 / 7776;
+        var pwl = 2611 / 7776;
+        var pll = 2275 / 7776;
+        return [pww, pwl, pll];
+    };
     RiskDiceProbabilities.prototype.getPd1 = function () {
-        var rt = this.pd1.clone();
+        var rt = this.pgd1.clone();
         return rt;
     };
     RiskDiceProbabilities.prototype.getPd2 = function () {
-        var rt = this.pd2.clone();
+        var rt = this.pgd2.clone();
         return rt;
     };
     RiskDiceProbabilities.prototype.getPd3 = function () {
-        var rt = this.pd3.clone();
+        var rt = this.pgd3.clone();
         return rt;
     };
     RiskDiceProbabilities.prototype.getPdd2 = function () {
-        var rt = this.pdd2.clone();
+        var rt = this.pgdd2.clone();
         return rt;
     };
     RiskDiceProbabilities.prototype.getPdd3 = function () {
-        var rt = this.pdd3.clone();
-        return rt;
-    };
-    RiskDiceProbabilities.prototype.get1v1Probs = function () {
-        var rt = this.probs1v1.clone();
-        return rt;
-    };
-    RiskDiceProbabilities.prototype.get1v2Probs = function () {
-        var rt = this.probs1v2.clone();
-        return rt;
-    };
-    RiskDiceProbabilities.prototype.get2v1Probs = function () {
-        var rt = this.probs2v1.clone();
-        return rt;
-    };
-    RiskDiceProbabilities.prototype.get2v2Probs = function () {
-        var rt = this.probs2v2.clone();
-        return rt;
-    };
-    RiskDiceProbabilities.prototype.get3v1Probs = function () {
-        var rt = this.probs3v1.clone();
-        return rt;
-    };
-    RiskDiceProbabilities.prototype.get3v2Probs = function () {
-        var rt = this.probs3v2.clone();
+        var rt = this.pgdd3.clone();
         return rt;
     };
     return RiskDiceProbabilities;
--- modules/RiskDiceProbabilities.ts
+++ modules/RiskDiceProbabilities.ts
@@ -7,33 +7,40 @@
 // import  RiskDiceProbabilities = require('../modules/RiskDiceProbabilities');
 // let probs = new RiskDiceProbabilities();
 //
-// let probs1v1 = probs.get1v1Probs();
-// let probs1v2 = probs.get1v2Probs();
-// let probs2v1 = probs.get2v1Probs();
-// let probs2v2 = probs.get2v2Probs();
-// let probs3v1 = probs.get3v1Probs();
-// let probs3v2 = probs.get3v2Probs();
+// let probs1v1 = probs.getProbs1v1();
+// let probs1v2 = probs.getProbs1v2();
+// let probs2v1 = probs.getProbs2v1();
+// let probs2v2 = probs.getProbs2v2();
+// let probs3v1 = probs.getProbs3v1();
+// let probs3v2 = probs.getProbs3v2();
 //
 
 
-// CONSTANTS FOR PROBABILITIES
+// RULES FOR DICE BATTLES
+// WHEN DEFENDER ONLY ROLLS 1 DIE
+// OR ATTACKER ONLY ROLLS 1 DIE:
 
-// The probablity constants given here are expressed as '216ths'. That is,
-// they represent numerators of fractions with a denominator of 216. This
-// number is 6 to the third power. Any probability value for one, two or
-// three dice will have a denominator which divides into this number.
+// Only one army can be lost in a contest where either the Attacker or the
+// Defender only rolls one die. If Attacker rolls 2 or 3 dice and Defender
+// only rolls one die, then Attacker's highest die is compared with the
+// Defender's roll. If Defender rolls 2 dice and Attacker only rolls
+// one die, then the Attacker's roll is compared with the Defender's highest
+// die. Defender loses one army if their result is lower than the Attacker's,
+// otherwise the Attacker loses one army ("Tie goes to the Defender").
 
-// In simpler terms, dividing one of these values by 216 will give the
-// probability that the term represents.
 
-// pp[0] for any of these arrays is a dummy value. pp[6] represents the
-// probability of getting the indicated number of 6's, pp[5] represents
-// probabilities for 5 or greater, etc. pp[7] is set to zero to make it
-// easy to access the correct probability of rolling a number higher than 6.
+// RULES FOR DICE BATTLES
+// WHEN DEFENDER ROLLS 2 DICE
+// AND ATTACKER ROLLS 2 OR 3 DICE:
 
-// The probabilities here were computed algebraicially, then confirmed by
-// Monte Carlo estimation using test scripts in this project folder.
-
+// A total of two armies will be lost for each roll. Either Defender will
+// lose 2 armies ('WW'), Defender and Attacker will each lose one army
+// ('WL'), or Attacker will lose 2 armies ('LL'). Results are computed by
+// first comparing Attacker's highest die with Defender's highest die, then
+// comparing Attacker's second highest to Defender's second highest. For
+// each comparision, Defender loses one army if their die is lower than
+// Attacker's die, and Attacker loses one army if their die is lower or
+// equal to Defender's die ("Tie goes to the Defender").
 
 
 class RiskDiceProbabilities {
@@ -44,17 +51,8 @@ class RiskDiceProbabilities {
   private pgdd2;
   private pgdd3;
 
-  private probs1v1;
-  private probs1v2;
-  private probs2v1;
-  private probs2v2;
-  private probs3v1;
-  private probs3v2;
-
-
 
   constructor() {
-
 
     // The probability of rolling d or greater when rolling one die is
     // represented by:  p[d] / 216
@@ -76,252 +74,93 @@ class RiskDiceProbabilities {
     // when rolling three dice is represented by:  pdd3[d] / 216
     let pgdd3: number[] = [0, 216, 200, 160, 108, 56, 16, 0];
 
-
-
-    // RULES FOR DICE BATTLES
-    // WHEN DEFENDER ONLY ROLLS 1 DIE
-    // OR ATTACKER ONLY ROLLS 1 DIE:
-
-    // Only one army can be lost in a contest where either the Attacker or the
-    // Defender only rolls one die. If Attacker rolls 2 or 3 dice and Defender
-    // only rolls one die, then Attacker's highest die is compared with the
-    // Defender's roll. If Defender rolls 2 dice and Attacker only rolls
-    // one die, then the Attacker's roll is compared with the Defender's highest
-    // die. Defender loses one army if their result is lower than the Attacker's,
-    // otherwise the Attacker loses one army ("Tie goes to the Defender").
-
-
-    // Probabilities for Attacker rolling 1 die and Defender rolling 1 die.
-
-    let probs1v1: number[][] = [
-      [],
-      [180, 36],
-      [144, 72],
-      [108, 108],
-      [72, 144],
-      [36, 180],
-      [0, 216]
-    ];
-
-
-    // Probabilities for Attacker rolling 1 die and Defender rolling 2 dice.
-
-    let probs1v2: number[][] = probs1v1;
-
-
-    // Probabilities for Attacker rolling 2 dice and Defender rolling 1 die.
-
-    let probs2v1: number[][] = [
-      [],
-      [210, 6],
-      [192, 24],
-      [162, 54],
-      [120, 96],
-      [66, 150],
-      [0, 216]
-    ];
-
-
-    // Probabilities for Attacker rolling 3 dice and Defender rolling 1 die.
-
-    let probs3v1: number[][] = [
-      [],
-      [215, 1],
-      [208, 8],
-      [189, 27],
-      [152, 64],
-      [91, 125],
-      [0, 216]
-    ];
-
-
-
-    // RULES FOR DICE BATTLES
-    // WHEN DEFENDER ROLLS 2 DICE
-    // AND ATTACKER ROLLS 2 OR 3 DICE:
-
-    // A total of two armies will be lost for each roll. Either Defender will
-    // lose 2 armies ('WW'), Defender and Attacker will each lose one army
-    // ('WL'), or Attacker will lose 2 armies ('LL'). Results are computed by
-    // first comparing Attacker's highest die with Defender's highest die, then
-    // comparing Attacker's second highest to Defender's second highest. For
-    // each comparision, Defender loses one army if their die is lower than
-    // Attacker's die, and Attacker loses one army if their die is lower or
-    // equal to Defender's die ("Tie goes to the Defender").
-
-
-    // Probabilities for Attacker rolling 2 dice and Defender rolling 2 dice.
-
-    let probs2v2: number[][][] = [
-      [],
-
-      [
-        [],
-        [150, 60, 6],
-      ],
-
-      [
-        [],
-        [144, 54, 18],
-        [96, 96, 24],
-      ],
-
-      [
-        [],
-        [126, 60, 30],
-        [90, 78, 48],
-        [54, 108, 54],
-      ],
-
-      [
-        [],
-        [96, 78, 42],
-        [72, 72, 72],
-        [48, 78, 90],
-        [24, 96, 96],
-      ],
-
-      [
-        [],
-        [54, 108, 54],
-        [42, 78, 96],
-        [30, 60, 126],
-        [18, 54, 144],
-        [6, 60, 150],
-      ],
-
-      [
-        [],
-        [0, 150, 66],
-        [0, 96, 120],
-        [0, 54, 162],
-        [0, 24, 192],
-        [0, 6, 210],
-        [0, 0, 216],
-      ]
-
-    ];
-
-
-    // Probabilities for Attacker rolling 3 dice and Defender rolling 2 dice.
-
-    let probs3v2: number[][][] = [
-      [],
-
-      [
-        [],
-        [200, 15, 1]
-      ],
-
-      [
-        [],
-        [196, 16, 4],
-        [160, 48, 8]
-      ],
-
-      [
-        [],
-        [180, 29, 7],
-        [153, 43, 20],
-        [108, 81, 27]
-      ],
-
-      [
-        [],
-        [146, 60, 10],
-        [128, 56, 32],
-        [98, 64, 54],
-        [56, 96, 64]
-      ],
-
-      [
-        [],
-        [88, 115, 13],
-        [79, 93, 44],
-        [64, 71, 81],
-        [43, 61, 112],
-        [16, 75, 125]
-      ],
-
-      [
-        [],
-        [0, 200, 16],
-        [0, 160, 56],
-        [0, 108, 108],
-        [0, 56, 160],
-        [0, 16, 200],
-        [0, 0, 216]
-      ]
-
-    ];
-
   }  // end of constructor()
 
 
   // Getter methods
 
+  // Probabilities for Attacker rolling 1 die and Defender rolling 1 die.
+
+  getProbs1v1() {
+    let pw: number = 15 / 36;
+    let pl: number = 21 / 36;
+    return [pw, pl];
+  }
+
+
+  // Probabilities for Attacker rolling 1 die and Defender rolling 2 dice.
+
+  getProbs1v2() {
+    let pw: number = 55 / 216;
+    let pl: number = 161 / 216;
+    return [pw, pl];
+  }
+
+
+  // Probabilities for Attacker rolling 2 dice and Defender rolling 1 die.
+
+  getProbs2v1() {
+    let pw: number = 125 / 216;
+    let pl: number = 91 / 216;
+    return [pw, pl];
+  }
+
+
+  // Probabilities for Attacker rolling 2 dice and Defender rolling 2 dice.
+
+  getProbs2v2() {
+    let pww: number = 295 / 1296;
+    let pwl: number = 420 / 1296;
+    let pll: number = 581 / 1296;
+    return [pww, pwl, pll];
+  }
+
+
+  // Probabilities for Attacker rolling 3 dice and Defender rolling 1 die.
+
+  getProbs3v1() {
+    let pw: number = 855 / 1296;
+    let pl: number = 441 / 1296;
+    return [pw, pl];
+  }
+
+
+  // Probabilities for Attacker rolling 3 dice and Defender rolling 2 dice.
+
+  getProbs3v2() {
+    let pww: number = 2890 / 7776;
+    let pwl: number = 2611 / 7776;
+    let pll: number = 2275 / 7776;
+    return [pww, pwl, pll];
+  }
+
+
   getPd1() {
-    let rt: number[][] = this.pd1.clone();
+    let rt: number[][] = this.pgd1.clone();
     return rt;
   }
 
 
   getPd2() {
-    let rt: number[][] = this.pd2.clone();
+    let rt: number[][] = this.pgd2.clone();
     return rt;
   }
 
 
   getPd3() {
-    let rt: number[][] = this.pd3.clone();
+    let rt: number[][] = this.pgd3.clone();
     return rt;
   }
 
 
   getPdd2() {
-    let rt: number[][] = this.pdd2.clone();
+    let rt: number[][] = this.pgdd2.clone();
     return rt;
   }
 
 
   getPdd3() {
-    let rt: number[][] = this.pdd3.clone();
-    return rt;
-  }
-
-
-  get1v1Probs() {
-    let rt: number[][] = this.probs1v1.clone();
-    return rt;
-  }
-
-
-  get1v2Probs() {
-    let rt: number[][] = this.probs1v2.clone();
-    return rt;
-  }
-
-
-  get2v1Probs() {
-    let rt: number[][] = this.probs2v1.clone();
-    return rt;
-  }
-
-
-  get2v2Probs() {
-    let rt: number[][][] = this.probs2v2.clone();
-    return rt;
-  }
-
-
-  get3v1Probs() {
-    let rt: number[][] = this.probs3v1.clone();
-    return rt;
-  }
-
-
-  get3v2Probs() {
-    let rt: number[][][] = this.probs3v2.clone();
+    let rt: number[][] = this.pgdd3.clone();
     return rt;
   }
 
