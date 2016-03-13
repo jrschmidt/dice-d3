7ae422456bfe592670a9f3a32bf7735ff77e64cd
JR Schmidt <jrschmidt56@gmail.com>
2016-03-12 17:23:04 -0800

    make chart with data from a separate js file

--- rdb-charts/d3x02.js
+++ rdb-charts/d3x02.js
@@ -3,29 +3,6 @@
 // Drawing the charts from pre-configured data
 
 
-var dataSpecs = {
-  'graphHeight': 3,
-  'graphDepth': 4
-};
-
-
-var dataNodes = [
-  {'type': 'root', 'att': 3, 'def': 1, 'a': 0, 'b': 3},
-  {'type': 'pw', 'att': 3, 'def': 0, 'a': 3, 'b': 1},
-  {'type': 'pl', 'att': 2, 'def': 1, 'a': 1, 'b': 4},
-  {'type': 'pw', 'att': 2, 'def': 0, 'a': 3, 'b': 3},
-  {'type': 'pl', 'att': 1, 'def': 1, 'a': 3, 'b': 5}
-];
-
-
-var dataLines = [
-  {'end0': [0, 3], 'end1': [3, 1], 'type': 'pw', 'probs': 0.579},
-  {'end0': [0, 3], 'end1': [1, 4], 'type': 'pl', 'probs': 0.421},
-  {'end0': [1, 4], 'end1': [3, 3], 'type': 'pw', 'probs': 0.417},
-  {'end0': [1, 4], 'end1': [3, 5], 'type': 'pl', 'probs': 0.583}
-];
-
-
 // Compute graph specs
 var dx = Math.floor(500 / dataSpecs.graphDepth);
 var x0 = Math.floor(dx / 2);
@@ -55,33 +32,12 @@ var topCaptions = svg.append('text')
   .enter()
   .append('line')
   .attr('class', 'line')
-  .attr('x1', function (d) { return 62 + 125 * d.end0[0]; })
-  .attr('y1', function (d) { return 100 * d.end0[1]; })
-  .attr('x2', function (d) { return 62 + 125 * d.end1[0]; })
-  .attr('y2', function (d) { return 100 * d.end1[1]; })
-  .style('stroke', 'red');
-
-
-// // Update Nodes
-// svg.selectAll('node')
-//   .data(dataNodes)
-//   .enter()
-//   .append('circle')
-//   .attr('class', 'node')
-//   .attr('cx', function (d) {
-//     return 25 + 50 * d[0];
-//   })
-//   .attr('cy', function (d) {
-//     return 275 - 50 * d[1];
-//   })
-//   .style('fill', '#669966')
-//   .attr('r', 6);
-
-
-  // Tooltip
-  // var tip = svg.append('text')
-  //   .attr('class', 'tooltip')
-  //   .style('font', '10px sans-serif')
-  //   .style('font-weight', 'bold')
-  //   .style('color', '#ffffff')
-  //   .style('opacity', 0);
+  .attr('x1', function (d) { return x0 + dx * d.end0[0]; })
+  .attr('y1', function (d) { return dy * d.end0[1]; })
+  .attr('x2', function (d) { return x0 + dx * d.end1[0]; })
+  .attr('y2', function (d) { return dy * d.end1[1]; })
+  .style('stroke', function(d) {
+    if (d.type == 'pw' || d.type == 'pww') {return '#339933';}
+    if (d.type == 'pl' || d.type == 'pll') {return '#993333';}
+    if (d.type == 'pwl') {return '#333399';}
+  });
